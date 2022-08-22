e2218db47382621709f03596b9640a58f8483056
TChukwuleta <tobechichukwuleta125@gmail.com>
2022-08-22 09:35:50 +0100

    new guys

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -4,61 +4,93 @@ const { authService, tokenService } = require("../services")
 const pick = require("../helpers/pick")
 
 const register = catchAsync(async function(req, res) {
-    const user = await authService.register(req.body)
-    const tokens = await tokenService.generateAuthTokens(user, true)
-    res.status(201).send({
-        message: "User registration was successful",
-        data: {
-            user,
-            token: tokens.access.token
-        }
-    })
+    try {
+        const user = await authService.register(req.body)
+        const tokens = await tokenService.generateAuthTokens(user, true)
+        res.status(201).send({
+            message: "User registration was successful",
+            data: {
+                user,
+                token: tokens.access.token
+            }
+        })
+    } catch (error) {
+        const errorCode = error.code || 500
+        const errorMessage = error.message || error
+        res.status(errorCode).send({
+            message: `User registration failed. ${errorMessage}`,
+        })
+    }
 })
 
 const login = catchAsync(async(req, res) => {
-    const { email, password } = req.body
-    const user = await authService.login(email, password)
-    const tokens = await tokenService.generateAuthTokens(user)
-    res.status(200).send({
-        message: "User login was successful",
-        data: {
-            user,
-            token: tokens.access.token
-        }
-    })
+    try {
+        const { email, password } = req.body
+        const user = await authService.login(email, password)
+        const tokens = await tokenService.generateAuthTokens(user)
+        res.status(200).send({
+            message: "User login was successful",
+            data: {
+                user,
+                token: tokens.access.token
+            }
+        })
+    } catch (error) {
+        const errorCode = error.code || 500
+        const errorMessage = error.message || error
+        res.status(errorCode).send({
+            message: `User registration failed. ${errorMessage}`,
+        })
+    }
 })
 
 const getUser = catchAsync(async (req, res) => {
-    let user
-    if(req.query.userId){
-        user = JSON.parse(
-            JSON.stringify(await authService.getUserById(req.query.userId))
-        )
-    } else {
-        user = JSON.parse(
-            JSON.stringify(await authService.getUserById(req.user._id))
-        );
+    try {
+        let user
+        if(req.query.userId){
+            user = JSON.parse(
+                JSON.stringify(await authService.getUserById(req.query.userId))
+            )
+        } else {
+            user = JSON.parse(
+                JSON.stringify(await authService.getUserById(req.user._id))
+            );
+        }
+        res.status(200).send({
+            message: "User details fetched successfully",
+            data: {
+            user,
+            },
+        });
+    } catch (error) {
+        const errorCode = error.code || 500
+        const errorMessage = error.message || error
+        res.status(errorCode).send({
+            message: `User registration failed. ${errorMessage}`,
+        })
     }
-    res.status(200).send({
-        message: "User details fetched successfully",
-        data: {
-          user,
-        },
-    });
 })
 
 const getUsers = catchAsync(async (req, res) => {
-    const options = pick(req.query, ["sortBy", "limit"]);
-    const users = await authService.getUsers({}, options)
-    const count = await authService.count()
-    res.status(200).send({
-        status: "success",
-        message: "Users Fetched successfully",
-        data: {
-          count,
-          users: JSON.parse(JSON.stringify(users)),
-        },
-    });
+    try {
+        const options = pick(req.query, ["sortBy", "limit"]);
+        const users = await authService.getUsers({}, options)
+        const count = await authService.count()
+        res.status(200).send({
+            status: "success",
+            message: "Users Fetched successfully",
+            data: {
+            count,
+            users: JSON.parse(JSON.stringify(users)),
+            },
+        });
+    } catch (error) {
+        const errorCode = error.code || 500
+        const errorMessage = error.message || error
+        res.status(errorCode).send({
+            message: `User registration failed. ${errorMessage}`,
+        })
+    }
 })
 
 module.exports = {
